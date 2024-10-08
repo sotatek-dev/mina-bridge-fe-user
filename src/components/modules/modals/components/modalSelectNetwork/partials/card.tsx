86aeb6b987dc2d60fdf7aff639da27569e4bac40
Sotatek-NamNguyen14 <nam.nguyen14@sotatek.com>
2024-10-08 14:39:49 +0700

    refactor: remove mina portal

--- src/components/modules/modals/components/modalSelectNetwork/partials/card.tsx
+++ src/components/modules/modals/components/modalSelectNetwork/partials/card.tsx
@@ -7,16 +7,15 @@ import {
   Image,
   Text,
 } from '@chakra-ui/react';
-import { useMemo, useRef } from 'react';
+import { useMemo } from 'react';
 
 import { useModalSNState } from '../context';
 
 import useNotifier from '@/hooks/useNotifier';
 import NETWORKS, { NETWORK_NAME } from '@/models/network';
-import { WALLET_NAME } from '@/models/wallet';
+import WALLETS, { WALLET_NAME } from '@/models/wallet';
 import {
   getPersistSlice,
-  getWalletInstanceSlice,
   getWalletSlice,
   useAppDispatch,
   useAppSelector,
@@ -24,25 +23,18 @@ import {
 import { persistSliceActions } from '@/store/slices/persistSlice';
 import { NETWORK_KEY, walletSliceActions } from '@/store/slices/walletSlice';
 
-
-
 type CardProps = { nwKey: NETWORK_NAME };
 
 export default function Card({ nwKey }: CardProps) {
   const dispatch = useAppDispatch();
-  const { networkName, isConnected } = useAppSelector(getWalletSlice);
-  const { walletInstance } = useAppSelector(getWalletInstanceSlice);
   const { lastNetworkName } = useAppSelector(getPersistSlice);
-  const { curNetworkKey } = useModalSNState().constants;
-  const {
-    hasSupportedNetwork,
-    handleCloseCurModal,
-    handleCloseLoadingModal,
-    handleOpenLoadingModal,
-  } = useModalSNState().methods;
+  const { networkName, isConnected } = useAppSelector(getWalletSlice);
 
-  const { sendNotification, checkNotifyActive } = useNotifier();
-  const curNotifyRef = useRef<any>(null);
+  const { curNetworkKey } = useModalSNState().constants;
+  const { hasSupportedNetwork, handleCloseCurModal, handleCloseLoadingModal } =
+    useModalSNState().methods;
+
+  const { sendNotification } = useNotifier();
 
   const network = NETWORKS[nwKey];
   const isSelected = useMemo(() => {
@@ -59,55 +51,39 @@ export default function Card({ nwKey }: CardProps) {
   async function handleSelectNetwork() {
     if (isSelected) return handleCloseCurModal();
 
-    if (!hasSupportedNetwork(nwKey)) {
-      switch (curNetworkKey) {
-        case NETWORK_KEY.SRC:
-          // prevent duplicate toast
-          if (
-            curNotifyRef.current !== null &&
-            checkNotifyActive(curNotifyRef.current)
-          )
-            return;
-
-          curNotifyRef.current = sendNotification({
-            toastType: 'warning',
-            options: {
-              title:
-                'Your wallet doesn\'t support this network. Please change the wallet connected',
-            },
-          });
-          return;
-        default:
-          return;
-      }
-    }
-    handleCloseCurModal();
-    if (walletInstance && walletInstance.name === WALLET_NAME.METAMASK) {
-      handleOpenLoadingModal();
-
-      // retry get wallet account
-      const res = await dispatch(
-        walletSliceActions.connectWallet({
-          wallet: walletInstance,
-          network: network,
-        })
-      );
-      //  when fail to connect
-      if (walletSliceActions.connectWallet.rejected.match(res)) {
-        sendNotification({
-          toastType: 'error',
-          options: {
-            title: res.error.message || null,
-          },
-        });
-        handleCloseLoadingModal();
-        return;
-      }
-      // when connect success
+    if (!isConnected) {
+      handleCloseCurModal();
       dispatch(persistSliceActions.setLastNetworkName(network.name));
+      return;
+    }
+
+    handleCloseCurModal();
+    await dispatch(walletSliceActions.disconnect());
+
+    // retry get wallet account
+    const res = await dispatch(
+      walletSliceActions.connectWallet({
+        wallet:
+          WALLETS[
+            network.name === NETWORK_NAME.ETHEREUM
+              ? WALLET_NAME.METAMASK
+              : WALLET_NAME.AURO
+          ]!,
+        network: network,
+      })
+    );
+    //  when fail to connect
+    if (walletSliceActions.connectWallet.rejected.match(res)) {
+      sendNotification({
+        toastType: 'error',
+        options: {
+          title: res.error.message || null,
+        },
+      });
       handleCloseLoadingModal();
       return;
     }
+    // when connect success
     dispatch(persistSliceActions.setLastNetworkName(network.name));
     return;
   }
@@ -119,12 +95,9 @@ export default function Card({ nwKey }: CardProps) {
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: '10px',
-    cursor: hasSupportedNetwork(nwKey) ? 'pointer' : 'not-allowed',
-    bg: !hasSupportedNetwork(nwKey)
-      ? 'text.100'
-      : !isSelected
-        ? 'text.500'
-        : 'linear-gradient(270deg, #DE622E 0%, #8271F0 100%)',
+    bg: !isSelected
+      ? 'text.500'
+      : 'linear-gradient(270deg, #DE622E 0%, #8271F0 100%)',
     mb: '22px',
     onClick: handleSelectNetwork,
     _active: {

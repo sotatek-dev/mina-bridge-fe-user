948469200778c3aaccc78fceada7f73a6d2ab92e
Sotatek-NamNguyen14 <nam.nguyen14@sotatek.com>
2025-01-24 16:47:23 +0700

    update: mina sc abi and cache

--- src/configs/ABIs/zk/Bridge.js
+++ src/configs/ABIs/zk/Bridge.js
@@ -121,6 +121,43 @@ export class Bridge extends SmartContract {
       )
     );
   }
+  // @method async unlock(
+  //   amount: UInt64,
+  //   receiver: PublicKey,
+  //   id: UInt64,
+  //   tokenAddr: PublicKey,
+  //   useSig1: Bool,
+  //   validator1: PublicKey,
+  //   sig1: Signature,
+  //   useSig2: Bool,
+  //   validator2: PublicKey,
+  //   sig2: Signature,
+  //   useSig3: Bool,
+  //   validator3: PublicKey,
+  //   sig3: Signature,
+  // ) {
+  //   const managerZkapp = new Manager(this.manager.getAndRequireEquals());
+  //   managerZkapp.isMinter(this.sender.getAndRequireSignature());
+  //   const msg = [
+  //     ...receiver.toFields(),
+  //     ...amount.toFields(),
+  //     ...tokenAddr.toFields(),
+  //   ]
+  //   this.validateValidator(
+  //     useSig1,
+  //     validator1,
+  //     useSig2,
+  //     validator2,
+  //     useSig3,
+  //     validator3,
+  //   );
+  //   this.validateSig(msg, sig1, validator1, useSig1);
+  //   this.validateSig(msg, sig2, validator2, useSig2);
+  //   this.validateSig(msg, sig3, validator3, useSig3);
+  //   const token = new FungibleToken(tokenAddr)
+  //   await token.mint(receiver, amount)
+  //   this.emitEvent("Unlock", new UnlockEvent(receiver, tokenAddr, amount, id));
+  // }
   async unlock(
     amount,
     receiver,
@@ -137,13 +174,13 @@ export class Bridge extends SmartContract {
     sig3
   ) {
     const managerZkapp = new Manager(this.manager.getAndRequireEquals());
-    managerZkapp.isAdmin(this.sender.getAndRequireSignature());
+    managerZkapp.isMinter(this.sender.getAndRequireSignature());
     const msg = [
       ...receiver.toFields(),
       ...amount.toFields(),
       ...tokenAddr.toFields(),
     ];
-    this.validateValidator(
+    await this.validateValidator(
       useSig1,
       validator1,
       useSig2,
@@ -151,9 +188,9 @@ export class Bridge extends SmartContract {
       useSig3,
       validator3
     );
-    this.validateSig(msg, sig1, validator1, useSig1);
-    this.validateSig(msg, sig2, validator2, useSig2);
-    this.validateSig(msg, sig3, validator3, useSig3);
+    await this.validateSig(msg, sig1, validator1, useSig1);
+    await this.validateSig(msg, sig2, validator2, useSig2);
+    await this.validateSig(msg, sig3, validator3, useSig3);
     const token = new FungibleToken(tokenAddr);
     await token.mint(receiver, amount);
     this.emitEvent('Unlock', new UnlockEvent(receiver, tokenAddr, amount, id));
@@ -167,43 +204,9 @@ export class Bridge extends SmartContract {
     validator3
   ) {
     let count = UInt64.from(0);
-    const zero = Field.from(0);
-    const falseB = Bool(false);
-    const trueB = Bool(true);
     const validatorManager = new ValidatorManager(
       this.validatorManager.getAndRequireEquals()
     );
-    const validateIndex = async (validator, useSig) => {
-      const index = await validatorManager.getValidatorIndex(validator);
-      const isGreaterThanZero = index.greaterThan(zero);
-      let isOk = Provable.if(
-        useSig,
-        Provable.if(isGreaterThanZero, trueB, falseB),
-        trueB
-      );
-      isOk.assertTrue('Public key not found in validators');
-    };
-    const notDupValidator12 = Provable.if(
-      useSig1.and(useSig2),
-      Provable.if(validator1.equals(validator2), falseB, trueB),
-      trueB
-    );
-    const notDupValidator13 = Provable.if(
-      useSig1.and(useSig3),
-      Provable.if(validator1.equals(validator3), falseB, trueB),
-      trueB
-    );
-    const notDupValidator23 = Provable.if(
-      useSig2.and(useSig3),
-      Provable.if(validator2.equals(validator3), falseB, trueB),
-      trueB
-    );
-    const isDuplicate = Provable.if(
-      notDupValidator12.and(notDupValidator13).and(notDupValidator23),
-      falseB,
-      trueB
-    );
-    isDuplicate.assertFalse('Duplicate validator keys');
     count = Provable.if(useSig1, count.add(1), count);
     count = Provable.if(useSig2, count.add(1), count);
     count = Provable.if(useSig3, count.add(1), count);
@@ -211,6 +214,15 @@ export class Bridge extends SmartContract {
       this.threshold.getAndRequireEquals(),
       'Not reached threshold'
     );
+    const index1 = await validatorManager.getValidatorIndex(validator1);
+    const index2 = await validatorManager.getValidatorIndex(validator2);
+    const index3 = await validatorManager.getValidatorIndex(validator3);
+    const isValid1 = Provable.if(useSig1, index1.equals(Field(1)), Bool(true));
+    const isValid2 = Provable.if(useSig2, index2.equals(Field(2)), Bool(true));
+    const isValid3 = Provable.if(useSig3, index3.equals(Field(3)), Bool(true));
+    isValid1.assertTrue('Validator1 has incorrect index');
+    isValid2.assertTrue('Validator2 has incorrect index');
+    isValid3.assertTrue('Validator3 has incorrect index');
   }
   async validateSig(msg, signature, validator, useSig) {
     let isValidSig = signature.verify(validator, msg);

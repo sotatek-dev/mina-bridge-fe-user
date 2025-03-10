7a9c389e8a5d5843a6489a9abac133b879f0e9c6
Sotatek-NamNguyen14 <nam.nguyen14@sotatek.com>
2025-03-10 17:22:12 +0700

    chore: update mina message

--- src/components/modules/formBridge/partials/form.bridgeAmount.tsx
+++ src/components/modules/formBridge/partials/form.bridgeAmount.tsx
@@ -134,6 +134,10 @@ const Content = forwardRef<FormBridgeAmountRef, Props>((props, ref) => {
       getWeb3Instance(nwProvider).eth.getGasPrice(),
     );
     if (error || !gasPrice) return '0';
+    console.log(
+      '🚀 ~ form.bridgeAmount ~ estimate ratio:',
+      process.env.NEXT_PUBLIC_ESTIMATE_PRICE_RATIO,
+    );
     const priceBN = new BigNumber(gasPrice?.toString() || '0').multipliedBy(
       process.env.NEXT_PUBLIC_ESTIMATE_PRICE_RATIO || 10,
     );
